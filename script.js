cdf887798450bc4c9a2f4c621ee9e01436af1747
snnairan <118011458+snnairan@users.noreply.github.com>
2023-06-10 21:22:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -35,12 +35,12 @@ function rot13(encodedStr) {
   let decodedArr = []; // Your Result goes here
   // Only change code below this line
 
-  return encodedStr + 13; //return decodedArr
+  return lookup.encodedStr; //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
+console.log(rot13("SERR YBIR? NPPVBWBO"));
 
 // Do not change this line
 window.rot13 = rot13;
