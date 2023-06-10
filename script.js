73d79e6919fd521213bb02b9e548ed67a40b3f4d
snnairan <118011458+snnairan@users.noreply.github.com>
2023-06-10 22:44:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function rot13(encodedStr) {
   let decodedArr = ""; // Your Result goes here
   // Only change code below this line
 	for(let i=0; i < encodedStr.length; i++){
-		if(lookup[encodedStr[i]] == undefined){
+		if(lookup[encodedStr[i]] === undefined){
 			decodedArr += encodedStr[i]
 		}
 		else{
